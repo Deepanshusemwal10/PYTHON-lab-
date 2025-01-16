35eaf0be9d352e9abf286cb73a5366e250e3dfd5
Deepanshusemwal10 <deepusemwal40@gmail.com>
2025-01-16 09:00:20 +0530

    Add files via upload

---
+++ labass 2.py
@@ -0,0 +1,20 @@
+x = "hello"
+print(x)
+integer_value = 42
+print("Integer value:", integer_value)
+float_value = 3.14
+print("Float value:", float_value)
+string_value = "Hello, World!"
+print("String value:", string_value)
+boolean_value = True
+print("Boolean value:", boolean_value)
+list_value = [1, 2, 3, 4, 5]
+print("List value:", list_value)
+tuple_value = (10, 20, 30)
+print("Tuple value:", tuple_value)
+dictionary_value = {"name": "Alice", "age": 25}
+print("Dictionary value:", dictionary_value)
+set_value = {100, 200, 300}
+print("Set value:", set_value)
+none_value = None
+print("NoneType value:", none_value)
